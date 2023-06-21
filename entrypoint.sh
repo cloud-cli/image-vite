5b1386f7830f645a40ea51652ff4da30d4957803
Darlan Alves <me@darlanalv.es>
2023-06-21 12:28:50 +0200

    fix: move dist folder after build

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,7 @@ case $1 in
     [ -f /home/app/index.html ] && cp /home/app/index.html /home/workspace/
     npm run ci || exit 1
     [ -d /home/app/assets ] && cp -r /home/app/assets/* /home/workspace/dist/
+    mv /home/workspace/dist /home/app
     ls -al /home/app
     ;;
 
