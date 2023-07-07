ee3a9c1c9bbeda92d6d9966ca50fa94aa80f7ec4
Darlan Alves <me@darlanalv.es>
2023-07-07 10:38:09 +0200

    chore: update merge-packages.js

--- workspace/merge-packages.js
+++ workspace/merge-packages.js
@@ -1,13 +1,19 @@
 const fs = require("fs");
 
-if (fs.existsSync("./app/package.json")) {
-  const left = JSON.parse(fs.readFileSync("/home/workspace/package.json", "utf8"));
-  const right = JSON.parse(fs.readFileSync("/home/app/package.json", "utf8"));
+const source = "/home/app/package.json";
+const target = "/home/workspace/package.json";
+
+if (fs.existsSync(source)) {  
+  const left = JSON.parse(fs.readFileSync(target, "utf8"));
+  const right = JSON.parse(fs.readFileSync(source, "utf8"));
 
   Object.assign(left.scripts, right.scripts || {});
   Object.assign(left.dependencies, right.dependencies || {});
   Object.assign(left.devDependencies, right.devDependencies || {});
+
   const pkg = JSON.stringify(left, null, 2);
-  fs.writeFileSync("/home/workspace/package.json", pkg);
+  fs.writeFileSync(target, pkg);
+  
+  console.log("Added project dependencies");
   console.log(pkg);
 }
