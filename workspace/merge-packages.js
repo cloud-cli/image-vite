4eab1cf3abafb84340f66ddb41e33361534e02e1
Darlan Alves <me@darlanalv.es>
2023-07-16 09:26:11 +0200

    fix: ensure dependencies are present on merge

--- workspace/merge-packages.js
+++ workspace/merge-packages.js
@@ -1,11 +1,13 @@
-const fs = require("fs");
+const fs = require('fs');
 
-const source = "/home/workspace/src/package.json";
-const target = "/home/workspace/package.json";
+const source = '/home/workspace/src/package.json';
+const target = '/home/workspace/package.json';
 
 if (fs.existsSync(source)) {
-  const left = JSON.parse(fs.readFileSync(target, "utf8"));
-  const right = JSON.parse(fs.readFileSync(source, "utf8"));
+  const left = JSON.parse(fs.readFileSync(target, 'utf8'));
+  const right = JSON.parse(fs.readFileSync(source, 'utf8'));
+
+  left.dependencies ||= {};
 
   Object.assign(left.scripts, right.scripts || {});
   Object.assign(left.dependencies, right.dependencies || {});
@@ -14,6 +16,6 @@ if (fs.existsSync(source)) {
   const pkg = JSON.stringify(left, null, 2);
   fs.writeFileSync(target, pkg);
 
-  console.log("Added project dependencies");
+  console.log('Added project dependencies');
   console.log(pkg);
 }
