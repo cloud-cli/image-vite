6033d77dff43da459935e88a932e24b950357c6c
Darlan Alves <me@darlanalv.es>
2024-01-22 00:55:23 +0100

    fix: align dev and build commands

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,9 @@ FROM ghcr.io/cloud-cli/node:latest
 
 USER root
 COPY workspace /home/workspace
+RUN cd /home/workspace && npm i --no-audit
 COPY vite-entrypoint.sh /home/node/vite-entrypoint.sh
-RUN cd /home/workspace && npm i --no-audit && chmod +x /home/node/vite-entrypoint.sh
+RUN chmod +x /home/node/vite-entrypoint.sh
 ENTRYPOINT ["/bin/bash", "/home/node/vite-entrypoint.sh"]
 
 ENV PATH "$PATH:/home/node/npm/bin:/home/app/node_modules/.bin:/home/workspace/node_modules/.bin"
--- vite-entrypoint.sh
+++ vite-entrypoint.sh
@@ -8,24 +8,30 @@ case $1 in
     echo "Building vite project"
     cd /home/workspace
     node merge-packages.js
-    cp .dockerignore $SRC
 
+    # add
+    cp .dockerignore $SRC
     [ -f "$SRC/superstatic.json" ] || cp superstatic.json $SRC
 
+    # pull custom files from source folder
     for file in index.html manifest.json superstatic.json; do
       [ -f "$SRC/$file" ] && cp $SRC/$file /home/workspace/
     done
 
+    # sort out extra assets
     npm run ci || exit 1
-    [ -d $SRC/assets ] && cp -r $SRC/assets/* /home/workspace/dist/
+    [ -d $SRC/assets ] && cp -r $SRC/assets /home/workspace/dist/assets
+    ls -al /home/workspace/*
     mv /home/workspace/dist $SRC/
-    ls -al $SRC
+    ls -al $SRC/dist
     ;;
 
   dev)
     cd /home/workspace
     [ -d $SRC/assets ] && ln -s $SRC/assets /home/workspace/assets
-    [ -f $SRC/index.html ] && cp $SRC/index.html /home/workspace/
+    [ -f $SRC/index.html ] && rm /home/workspace/index.html && ln -s $SRC/index.html /home/workspace/
+    ls -al /home/workspace
+    npm i
     npm run dev
     ;;
 
@@ -33,7 +39,8 @@ case $1 in
     cd /home/workspace;
     node merge-packages.js
     echo "Running preview server"
-    npm i && npm run preview -- --host 0.0.0.0 --port $PORT
+    npm i
+    npm run preview
     ;;
 
   *)
